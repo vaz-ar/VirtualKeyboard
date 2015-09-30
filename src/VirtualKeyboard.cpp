6d7fb0f51ecfb2a1011880c85e27677989a1663e
Arnaud Vazard <arnaud.vazard@tecofrance.com>
2015-09-30 13:53:37 +0200

    Fix missing ";" in the punctuation list

--- src/VirtualKeyboard.cpp
+++ src/VirtualKeyboard.cpp
@@ -182,7 +182,7 @@ bool VirtualKeyboard::initialisationKeymaps(QString s_language)
                                << "," << "-" << "_" << "[" << "]" << "?" << ".";
 
     this->mlists_punctuationKeymap << "!" << "@" << "#" << "$" << "%" << "&&" << "*" << "(" << ")" << ""
-                                   << "," << "-" << "_" << "[" << "]" << "?" << "."  << "/" << "\\";
+                                   << ";" << "-" << "_" << "[" << "]" << "?" << "."  << "/" << "\\";
 
     // If the language is not EN or FR then return false
     if (s_language == "EN")
